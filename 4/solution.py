8298525eddcadf289440588cf31be830f0bb7f07
rain <brother.rain.1024@gmail.com>
2015-11-03 15:17:10 +0700

    add solution for 4

---
+++ 4/solution.py
@@ -0,0 +1,19 @@
+from math import sqrt, ceil
+
+__author__ = 'rain'
+
+def is_palindrome(n):
+    number_in_string = str(n)
+    return number_in_string == number_in_string[::-1]
+
+def solve():
+    max = 0
+    for i in range(999, 100, -1):
+        for j in range(999, 100, -1):
+            product = i * j
+            if product > max and is_palindrome(product):
+                max = product
+    return max
+
+
+print solve()
