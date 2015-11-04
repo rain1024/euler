4dbd9d78a075a621b5ba2dbeee13ca1c3985d01a
rain <brother.rain.1024@gmail.com>
2015-11-04 10:55:46 +0700

    rename folder

--- 009/solution.py
+++ 009/solution.py
@@ -8,10 +8,6 @@ def solve():
         for b in range(1, 500):
             for c in range(1, 500):
                 if(a + b + c == 1000) and (a*a + b*b == c*c):
-                    print a
-                    print b
-                    print c
-                    print a* b * c
                     return a * b * c
 
 print solve()
