bbde85d78e7adcb437c203d33e08d8ba1a3b5044
rain <brother.rain.1024@gmail.com>
2015-11-09 17:28:01 +0700

    refactor

--- 018/solution.py
+++ 018/solution.py
@@ -1,18 +1,16 @@
 __author__ = 'rain'
 
 import timeit
+from itertools import imap
 
 
 def get_node_label(i, j):
     return str(i) + "." + str(j)
 
 
-def find_max_local(x, y):
-    return map(lambda i: max(x[i], x[i + 1]) + y[i], range(len(y)))
-
-
 def solve():
-    from itertools import imap
+    f = lambda above, below, below_shift: above + max(below, below_shift)
+    find_max_local = lambda below, above: list(imap(f, above, below, below[1:]))
     data = [map(int, row.split()) for row in open("triangle.txt")][::-1]
     return reduce(find_max_local, data)[0]
 
